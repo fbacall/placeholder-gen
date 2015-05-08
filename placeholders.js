380aeb5751e40b0a8acef3297ee3ffe1c9fbe93c
Finn Bacall <finn.bacall@manchester.ac.uk>
2015-05-08 17:14:09 +0100

    Made text size more sensible for portrait images

--- placeholders.js
+++ placeholders.js
@@ -1,13 +1,13 @@
 var http = require('http');
-
 var port = parseInt(process.argv[2] || '3000');
+var defaults = { width: 500, height: 100, colour: '663399' };
 
-var defaults = { width: 200, height: 50, colour: '663399' };
+// Choose black or white text to contrast with the background colour
 function defaultTextColour(bgColour) {
   return (
           (
-           parseInt(bgColour.substr(0,2), 16) + 
-           parseInt(bgColour.substr(2,2), 16) + 
+           parseInt(bgColour.substr(0,2), 16) +
+           parseInt(bgColour.substr(2,2), 16) +
            parseInt(bgColour.substr(4,2), 16)
           ) / (3 * 0xFF)
          ) >= 0.5 ? '000000' : 'FFFFFF';
@@ -22,10 +22,11 @@ http.createServer(function (req, res) {
   var height = size[1] || defaults.height;
   var colour = params[2] || defaults.colour;
   var textColour = params[3] || defaultTextColour(colour);
+  var textSize = Math.ceil(Math.min((width / params[1].length), (height / 2)));
 
   var svg = '<svg xmlns="http://www.w3.org/2000/svg" width="'+width+'" height="'+height+'"><g>'+
               '<rect width="100%" height="100%" style="fill:#'+colour+'"/>'+
-              '<text y="50%" x="50%" text-anchor="middle" dominant-baseline="central" style="font-family: sans-serif; font-size: '+Math.ceil(height/4)+'px; fill:#'+textColour+'">'+width+'x'+height+'</text>' +
+              '<text y="50%" x="50%" text-anchor="middle" dominant-baseline="central" style="font-family: sans-serif; font-size: '+textSize+'px; fill:#'+textColour+'">'+width+'x'+height+'</text>' +
             '</g></svg>';
 
   res.writeHead(200, {'Content-Type': 'image/svg+xml', 'Content-Length': svg.length + 1});
