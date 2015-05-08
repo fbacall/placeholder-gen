c945c844c8e85fe1fd3197e10587409cc3fd4636
Finn Bacall <finn.bacall@manchester.ac.uk>
2015-05-08 08:53:51 +0100

    Use vanilla node

--- placeholders.js
+++ placeholders.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var app = express();
+var http = require('http');
 
-var port = process.argv[2] || 3000;
+var port = parseInt(process.argv[2] || '3000');
 
+var defaults = { width: 200, height: 50, colour: '663399' };
 function defaultTextColour(bgColour) {
   return (
           (
@@ -13,25 +13,24 @@ function defaultTextColour(bgColour) {
          ) >= 0.5 ? '000000' : 'FFFFFF';
 }
 
-app.get('/:size/:colour?/:text?', function(req, res) {
-  console.log(req.ip, req.params);
+http.createServer(function (req, res) {
+  var params = req.url.split('/');
+  console.log(req.connection.remoteAddress, params);
 
-  var size = req.params.size.split('x');
-  var width = size[0], height = size[1];
-  var colour = req.params.colour || '663399';
-  var textColour = req.params.text || defaultTextColour(colour);
+  var size = params[1].split('x');
+  var width = size[0] || defaults.width;
+  var height = size[1] || defaults.height;
+  var colour = params[2] || defaults.colour;
+  var textColour = params[3] || defaultTextColour(colour);
 
-  var svg = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="'+width+'" height="'+height+'"> \
-               <g> \
-                 <rect width="'+width+'" height="'+height+'" style="fill:#'+colour+'"/> \
-                 <text y="50%" x="50%" text-anchor="middle" dominant-baseline="central" style="font-family: sans-serif; font-size: '+Math.ceil(height/2)+'px; fill:#'+textColour+'">'+width+'x'+height+'</text> \
-               </g> \
-             </svg>';
+  var svg = '<svg xmlns="http://www.w3.org/2000/svg" width="'+width+'" height="'+height+'"><g>'+
+              '<rect width="100%" height="100%" style="fill:#'+colour+'"/>'+
+              '<text y="50%" x="50%" text-anchor="middle" dominant-baseline="central" style="font-family: sans-serif; font-size: '+Math.ceil(height/4)+'px; fill:#'+textColour+'">'+width+'x'+height+'</text>' +
+            '</g></svg>';
 
-  res.set('Content-Type', 'image/svg+xml');
-  res.send(svg);
-});
+  res.writeHead(200, {'Content-Type': 'image/svg+xml', 'Content-Length': svg.length + 1});
+  res.end(svg + '\n');
+}).listen(port);
 
-app.listen(port);
 console.log("Serving on port:", port);
 
