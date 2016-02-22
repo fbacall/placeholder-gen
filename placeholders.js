95d1af72d47aa25337a4f91464b4e1975ad49492
Finn Bacall <finn.bacall@manchester.ac.uk>
2016-02-22 13:39:03 +0000

    Limit PNG size

--- placeholders.js
+++ placeholders.js
@@ -3,6 +3,7 @@ var Rsvg = require('librsvg').Rsvg;
 var Stream = require('stream');
 
 var port = parseInt(process.argv[2] || '3000');
+var pngDimensionLimit = 1024;
 
 // Generate a randomish colour based on the given width and height
 function defaultBackgroundColour(width, height) {
@@ -22,9 +23,20 @@ Http.createServer(function (req, res) {
   var params = req.url.split('/');
   console.log(req.connection.remoteAddress, params);
 
+  // Check for .png extension
+  var segments = req.url.split('.');
+  var isPng = segments[segments.length - 1] && segments[segments.length - 1] == 'png';
+
   var size = params[1].split('x');
   var width = parseInt(size[0]) || Math.ceil(50 + (Math.random() * 500));
   var height = parseInt(size[1]) || Math.ceil(50 + (Math.random() * 200));
+
+  // Limit size of PNG
+  if(isPng) {
+    width = width > pngDimensionLimit ? pngDimensionLimit : width;
+    height = height > pngDimensionLimit ? pngDimensionLimit : height;
+  }
+
   var colour = params[2] || defaultBackgroundColour(width, height);
   var textColour = params[3] || defaultTextColour(colour);
   var text = params[4] ? unescape(params[4]) : '' + width + 'x' + height;
@@ -35,13 +47,11 @@ Http.createServer(function (req, res) {
               '<text y="50%" x="50%" text-anchor="middle" dominant-baseline="central" style="font-family: sans-serif; font-size: '+textSize+'px; fill:#'+textColour+'">'+text+'</text>' +
             '</g></svg>';
 
-  // Check for .png extension
-  var segments = req.url.split('.');
-  if(segments[segments.length - 1] && segments[segments.length - 1] == 'png') {
+  if(isPng) {
     var rsvg = new Rsvg();
-      var stream = new Stream.Readable();
-      stream.push(svg);
-      stream.push(null);
+    var stream = new Stream.Readable();
+    stream.push(svg);
+    stream.push(null);
 
     rsvg.on('finish', function() {
       var png = rsvg.render({ format: 'png', width: width, height: height }).data;
